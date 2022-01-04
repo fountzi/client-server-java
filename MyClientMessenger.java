05891512fd53a4cacfd5f4b74f4f69341799dd38
fountzi <fountzi1@gmail.com>
2022-01-04 15:10:28 +0200

    Client Interface

---
+++ MyClientMessenger.java
@@ -0,0 +1,14 @@
+public class MyClientMessenger
+{
+    public static void main(String args[])
+    {
+        MyMessageManger messageManager;
+        if(args.lenght==0) messageManager= new MySocketMessageManger("localhost");
+        else messageManager = new MySocketMessageManager(args[0]);
+
+        Client_interface clientGUI= new Client_interface(messageManager);
+        clientGUI.setSize(300,400);
+        clientGUI.setResizable(false);
+        clientGUI.setVisible(true);
+    }
+}
